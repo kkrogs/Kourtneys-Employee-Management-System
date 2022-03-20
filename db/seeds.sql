3ea7cd24cd386af82c3d15e2ab60e7ff42c23cf8
kkrogs <kourtney.krogman@gmail.com>
2022-03-20 14:57:25 -0600

    added to seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,31 +1,31 @@
 INSERT INTO department (name)
 VALUES 
 ('IT'),
-('Finance & Accounting'),
-('Sales & Marketing'),
-('Operations');
+('Company Operations'),
+('Sales'),
+('Management');
 
 INSERT INTO role (title, salary, department_id)
 VALUES
-('Full Stack Developer', 80000, 1),
-('Software Engineer', 120000, 1),
-('Accountant', 10000, 2), 
-('Finanical Analyst', 150000, 2),
-('Marketing Coordindator', 70000, 3), 
-('Sales Lead', 90000, 3),
-('Project Manager', 100000, 4),
-('Operations Manager', 90000, 4);
+('CEO', 200000, 2),
+('Vice President', 180000, 2),
+('Operations Manager', 150000, 2), 
+('Manager', 125000, 4),
+('Software Engineer', 110000, 1), 
+('Salesperson', 100000, 3),
+('Project Manager', 98000, 4),
+('Support', 90000, 1);
 
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES 
-('Mark', 'Miller', 2, null),
-('Devin', 'Anderson', 1, 1),
-('Mary', 'Brown', 4, null),
-('Ashley', 'Jones', 3, 3),
-('Tyler', 'Moore', 6, null),
-('Ana', 'Sanchez', 5, 5),
-('Lewis', 'Allen', 7, null),
-('Katherine', 'Green', 8, 7);
+('Anne', 'Hathaway', 2, null),
+('Nina', 'Simone', 1, 1),
+('Jessica', 'Simpson', 4, null),
+('Kelly', 'Jensen', 3, 3),
+('Melanie', 'Johnson', 6, null),
+('Maria', 'Santos', 5, 5),
+('Rachel', 'Peters', 7, null),
+('Jenna', 'Smith', 8, 7);
 
 
